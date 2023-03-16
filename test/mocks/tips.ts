2046a35ceeccecda5285c400960e83ed160999aa
Mathieu Bonhommeau <m.bonhommeau@unlimitedpix.com>
2023-03-16 20:51:55 +0100

    feat: split app.js with server for resolve conflict in tests

--- test/mocks/tips.ts
+++ test/mocks/tips.ts
@@ -1,8 +1,8 @@
 import TipsFactory from '../../src/application/factories/tipsFactory'
 
 export const tipsMocks = [
-    TipsFactory.create('sudo systemctl start postgres', 'Démarrer service postgres', new Date(), new Date()),
-    TipsFactory.create('sudo systemctl stop postgres', 'Stopper service postgres', new Date(), new Date()),
-    TipsFactory.create('sudo systemctl status postgres', 'Etat service postgres', new Date(), new Date()),
+    TipsFactory.create('sudo systemctl start postgres', 'Démarrer service postgres', 'PostgreSql', new Date(), new Date()),
+    TipsFactory.create('docker stop $(docker ps -a -q)', 'Stopper service postgres', 'Docker', new Date(), new Date()),
+    TipsFactory.create('sudo systemctl status postgres', 'Etat service postgres', 'PostgreSql', new Date(), new Date()),
 ]
 
